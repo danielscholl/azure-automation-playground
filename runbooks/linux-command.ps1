c1449850cc900be195c6bb637d2ad5f5438c6dae
danielscholl <daniel@scholllopez.com>
2017-09-03 14:52:53 -0500

    Updated linux-command.ps1 in Azure Automation.

--- runbooks/linux-command.ps1
+++ runbooks/linux-command.ps1
@@ -1,26 +1,52 @@
-﻿<#
+<#
     .DESCRIPTION
-        Play with executing commands over SSH.
+        This runbook allows commands to be exectued on a linux server.
+
+    .PARAMETER ResourceGroupName
+        Name of the Azure Resource Group containing the VMs to be started.
 
     .NOTES
         AUTHOR: Daniel Scholl
 #>
-workflow linux-command
-{
-    $Computer = "vm0"
+Param(
+    [Parameter(Mandatory = $true)]
+    [String] $ResourceGroup,
+    [Parameter(Mandatory = $true)]
+    [String] $MachineName
+)
 
-    $SSHCred = Get-AutomationPSCredential -Name 'LinuxCredential'
-    $Connection = Get-AutomationConnection -Name 'LinuxConnection'
-    $SSHKey = Get-AutomationVariable -Name 'SSHKey'
-
-
-    Invoke-SSHCommand `
-		-ComputerName $Connection `
-		-Credential $SSHCred `
-		[-KeyString $SSHKey `
-		[-Port 22] `
-		-ScriptBlock {
-            cd /bin
-            ls
-        }
+$ConnectionName = 'AzureRunAsConnection'
+$Conn = Get-AutomationConnection -Name $ConnectionName
+if (!$Conn) {
+    throw "Could not find an Connection Asset named '${ConnectionName}'."
 }
+
+Try {
+    # Authenticate
+    Add-AzureRMAccount -ServicePrincipal `
+        -Tenant $Conn.TenantID `
+        -ApplicationId $Conn.ApplicationID `
+        -CertificateThumbprint $Conn.CertificateThumbprint
+}
+Catch {
+    $ErrorMessage = 'Login to Azure failed.'
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Error: '
+    $ErrorMessage += $_
+    Write-Error -Message $ErrorMessage `
+        -ErrorAction Stop
+}
+
+# Get Machine Credential
+$automation = Get-AzureRmAutomationAccount -ResourceGroupName $ResourceGroup
+$credential = Get-AzureRmAutomationCredential -Name SSHCred `
+    -ResourceGroup $ResourceGroup `
+    -AutomationAccountName $automation.AutomationAccountName `
+
+# Get IP address of Machine    
+$vm = Get-AzureRMVM -Name $MachineName -ResourceGroup $ResourceGroup
+$ipconfig = Get-AzureRmNetworkInterface | Where-Object { $_.Id -eq $vm.NetworkInterfaceIDs[0]} | Get-AzureRmNetworkInterfaceIpConfig
+$publicIp = Get-AzureRmPublicIpAddress | Where-Object { $_.Id -eq $ipconfig.PublicIpAddress.Id } |Select-Object IpAddress
+
+
+# Write-Host $vm
